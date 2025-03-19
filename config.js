f0cd37dba23fbaaeb8d12c7be9e6249d4aab2f2c
sampath933 <89699004+sampath933@users.noreply.github.com>
2025-03-19 09:57:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=W2wSwCIA#frdAnaFX64l2NpNs5fE3S0UDEjBef7OlyxsUMOE7EtI' : process.env.SESSION_ID,
 };
